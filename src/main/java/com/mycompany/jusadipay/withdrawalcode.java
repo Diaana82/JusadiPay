40984a1b77db9bf25ceb566728b37f0e555fffbd
panshor <juanjodc79@gmail.com>
2025-11-08 16:13:08 -0500

    cree clase main y clase withdrawalcode

---
+++ src/main/java/com/mycompany/jusadipay/withdrawalcode.java
@@ -0,0 +1,77 @@
+
+package com.mycompany.jusadipay;
+import java.time.LocalDateTime;
+
+public class withdrawalcode {
+    private String codigo;
+    private LocalDateTime fechageneracion;
+    private LocalDateTime fechaexpiracion;
+    private String estado;
+    private User cuentaAsociante;
+    private String puntoretiro;
+
+    public withdrawalcode() {
+    }
+
+    public withdrawalcode(String codigo, LocalDateTime fechageneracion, LocalDateTime fechaexpiracion, String estado, User cuentaAsociante, String puntoretiro) {
+        this.codigo = codigo;
+        this.fechageneracion = fechageneracion;
+        this.fechaexpiracion = fechaexpiracion;
+        this.estado = estado;
+        this.cuentaAsociante = cuentaAsociante;
+        this.puntoretiro = puntoretiro;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDateTime getFechageneracion() {
+        return fechageneracion;
+    }
+
+    public void setFechageneracion(LocalDateTime fechageneracion) {
+        this.fechageneracion = fechageneracion;
+    }
+
+    public LocalDateTime getFechaexpiracion() {
+        return fechaexpiracion;
+    }
+
+    public void setFechaexpiracion(LocalDateTime fechaexpiracion) {
+        this.fechaexpiracion = fechaexpiracion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public User getCuentaAsociante() {
+        return cuentaAsociante;
+    }
+
+    public void setCuentaAsociante(User cuentaAsociante) {
+        this.cuentaAsociante = cuentaAsociante;
+    }
+
+    public String getPuntoretiro() {
+        return puntoretiro;
+    }
+
+    public void setPuntoretiro(String puntoretiro) {
+        this.puntoretiro = puntoretiro;
+    }
+    
+    
+    
+}
+
+
