44794100920af86cea42e10579ac4ba782e1d830
Sara Marin Motato <marin.sara1007@gmail.com>
2025-11-25 00:49:23 -0500

    Constructores, setters y getters creados para CodigoDeRetiro, Notificacion, TipoTransaccion y Transaccion

--- src/main/java/com/Project/project/model/CodigoDeRetiro.java
+++ src/main/java/com/Project/project/model/CodigoDeRetiro.java
@@ -1,4 +1,62 @@
 package com.Project.project.model;
 
+import java.time.LocalDateTime;
+
 public class CodigoDeRetiro {
+    private String codigo;
+    private LocalDateTime fechaGeneracion;
+    private LocalDateTime fechaExpiracion;
+    private String estado;
+    private Usuario usuarioAsociante;
+
+    public CodigoDeRetiro() {
+    }
+
+    public CodigoDeRetiro(String codigo, LocalDateTime fechaGeneracion, LocalDateTime fechaExpiracion, String estado, Usuario usuarioAsociante) {
+        this.codigo = codigo;
+        this.fechaGeneracion = fechaGeneracion;
+        this.fechaExpiracion = fechaExpiracion;
+        this.estado = estado;
+        this.usuarioAsociante = usuarioAsociante;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDateTime getFechaGeneracion() {
+        return fechaGeneracion;
+    }
+
+    public void setFechaGeneracion(LocalDateTime fechaGeneracion) {
+        this.fechaGeneracion = fechaGeneracion;
+    }
+
+    public LocalDateTime getFechaExpiracion() {
+        return fechaExpiracion;
+    }
+
+    public void setFechaExpiracion(LocalDateTime fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Usuario getUsuarioAsociante() {
+        return usuarioAsociante;
+    }
+
+    public void setUsuarioAsociante(Usuario usuarioAsociante) {
+        this.usuarioAsociante = usuarioAsociante;
+    }
 }
--- src/main/java/com/Project/project/model/Notificacion.java
+++ src/main/java/com/Project/project/model/Notificacion.java
@@ -1,4 +1,82 @@
 package com.Project.project.model;
 
+import java.time.LocalDateTime;
+
 public class Notificacion {
+    private String idNotificacion;
+    private Usuario usuarioDestino;
+    private String contenido;
+    private LocalDateTime fechaEnvio;
+    private String tipoNotificacion;
+    private Transaccion transaccionAsociada;
+    private CodigoDeRetiro codigoRetiroAsociado;
+
+    public Notificacion() {
+    }
+
+    public Notificacion(String idNotificacion, Usuario usuarioDestino, String contenido, LocalDateTime fechaEnvio, String tipoNotificacion, Transaccion transaccionAsociada, CodigoDeRetiro codigoRetiroAsociado) {
+        this.idNotificacion = idNotificacion;
+        this.usuarioDestino = usuarioDestino;
+        this.contenido = contenido;
+        this.fechaEnvio = fechaEnvio;
+        this.tipoNotificacion = tipoNotificacion;
+        this.transaccionAsociada = transaccionAsociada;
+        this.codigoRetiroAsociado = codigoRetiroAsociado;
+    }
+
+    public String getIdNotificacion() {
+        return idNotificacion;
+    }
+
+    public void setIdNotificacion(String idNotificacion) {
+        this.idNotificacion = idNotificacion;
+    }
+
+    public Usuario getUsuarioDestino() {
+        return usuarioDestino;
+    }
+
+    public void setUsuarioDestino(Usuario usuarioDestino) {
+        this.usuarioDestino = usuarioDestino;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public LocalDateTime getFechaEnvio() {
+        return fechaEnvio;
+    }
+
+    public void setFechaEnvio(LocalDateTime fechaEnvio) {
+        this.fechaEnvio = fechaEnvio;
+    }
+
+    public String getTipoNotificacion() {
+        return tipoNotificacion;
+    }
+
+    public void setTipoNotificacion(String tipoNotificacion) {
+        this.tipoNotificacion = tipoNotificacion;
+    }
+
+    public Transaccion getTransaccionAsociada() {
+        return transaccionAsociada;
+    }
+
+    public void setTransaccionAsociada(Transaccion transaccionAsociada) {
+        this.transaccionAsociada = transaccionAsociada;
+    }
+
+    public CodigoDeRetiro getCodigoRetiroAsociado() {
+        return codigoRetiroAsociado;
+    }
+
+    public void setCodigoRetiroAsociado(CodigoDeRetiro codigoRetiroAsociado) {
+        this.codigoRetiroAsociado = codigoRetiroAsociado;
+    }
 }
