e9c68846512c40ee24e1a731db9ef403f6fc8260
Diana Cristina Patiño Ramirez <patinoramirezdianacristina82@gmail.com>
2025-11-24 21:47:26 -0500

    Delete src/main/java/com/mycompany/jusadipay directory

--- src/main/java/com/mycompany/jusadipay/polimorfismo/OperacionCuenta.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.jusadipay.polimorfismo;
-
-import com.mycompany.jusadipay.Account;
-import com.mycompany.jusadipay.Transaction;
-
-public interface OperacionCuenta {
-     Transaction ejecutar(Account cuenta);
-}
