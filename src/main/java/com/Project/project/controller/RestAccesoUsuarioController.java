6522311838ab2b23226027c1968a37837fecf18a
panshor <juanjodc79@gmail.com>
2025-11-25 19:36:30 -0500

    Main

--- src/main/java/com/Project/project/controller/RestAccesoUsuarioController.java
+++ src/main/java/com/Project/project/controller/RestAccesoUsuarioController.java
@@ -8,6 +8,6 @@ public class RestAccesoUsuarioController {
 
     @GetMapping
     public String Inicio(){
-        return "Esta es la página de acceso Usuarios";
+        return "Esta es la página de Acceso Usuarios";
     }
 }
